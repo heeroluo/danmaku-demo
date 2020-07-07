1bdf04fa4874abe1a67d1a3f47384f53bfab4748
Heero.Law <heeroluo@gmail.com>
2020-07-07 14:40:01 +0800

    修正构建路径

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
   output: {
     filename: '[name].[hash].js',
     path: path.resolve(__dirname, '../dist/assets'),
-    publicPath: '/assets/'
+    publicPath: '/danmaku-demo/assets/'
   },
 
   resolve: {
